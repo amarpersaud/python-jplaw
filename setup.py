94fed076dd9922a7443c83efdf081b69a6b05362
Amar Persaud <tehspartaa@gmail.com>
2023-06-17 16:49:11 -0400

    Update setup and add license

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='plaw',
+    name='jplaw',
     version='0.1',
-    packages=['plaw'],
-    description='A python wrapper for the lemmy HTTP API',
-    author='Benjamin Jablonski',
-    author_email='thebigjablonski@gmail.com',
+    packages=['jplaw'],
+    description='A python wrapper for the lemmy HTTP API. Forked from plaw by Benjamin Jablonski (benja810)',
+    author='Amar Persaud',
+    author_email='amar.d.persaud@gmail.com',
     install_requires=["requests >=2.6.0, <3.0"],
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -18,5 +18,5 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
-    keywords='lemmy plaw api',
+    keywords='lemmy plaw api jplaw',
 )
