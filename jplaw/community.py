a2ff04a69a8185261a6405181c874fa735766827
Amar Persaud <tehspartaa@gmail.com>
2023-06-17 21:36:41 -0400

    Remove explicit community and post classes

--- jplaw/community.py
+++
@@ -1,6 +0,0 @@
-class Community:
-    def __init__(self, lem, instance, community_name):
-        self.lem = lem
-        self.instance = instance
-        self.community_name = community_name
-        self.json = lem.getCommunity(instance, community_name)
--- jplaw/post.py
+++
@@ -1,8 +0,0 @@
-class Post:
-    def __init__(self, lem, instance, cid, pid, author, text):
-        self.lem = lem
-        self.instance = instance
-        self.cid = cid
-        self.pid = pid
-        sef.author = author
-        self.text = text
