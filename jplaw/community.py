b0f81d30f5c52074f933bd681a64a4b5829ff20b
Amar Persaud <tehspartaa@gmail.com>
2023-06-18 18:30:06 -0400

    Fix dict and default to auth for communities

--- jplaw/community.py
+++ jplaw/community.py
@@ -1,18 +1,18 @@
 from .requestor import Requestor, HttpType
 from .api_paths import *
 
-def getCommunity(self, name, instance=None, auth=False, auth_token=None):
+def getCommunity(self, name, instance=None, auth=True, auth_token=None):
     url = self.apiURL(instance, "getCommunity")
     form = {"name": name}
     if(auth):
-        form.auth = auth_token or self.auth_token
+        form["auth"] = auth_token or self.auth_token
     res = self._req.request(HttpType.GET, url, form)
     return res["community_view"]["community"]
 
-def listCommunities(self, instance=None, auth=False, auth_token=None): 
+def listCommunities(self, instance=None, auth=True, auth_token=None): 
     url = self.apiURL(self.instance, "listCommunities")
     form={}
     if(auth):
-        form.auth = auth_token or self.auth_token
+        form["auth"] = auth_token or self.auth_token
     res = self._req.request(HttpType.GET, url, form)
     return res["communities"]
