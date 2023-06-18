26d210bf34751103f26747e33cd13af41f9c1dfc
Amar Persaud <tehspartaa@gmail.com>
2023-06-18 18:29:35 -0400

    Change data to form

--- jplaw/comment.py
+++ jplaw/comment.py
@@ -4,7 +4,7 @@ from .api_paths import *
 def submitComment(self, post_id, content, parent_id=None, instance=None, title=None, body=None, remoteinstance=None, nsfw=None, language_id=None, auth_token=None):
     url = self.apiURL(instance, "submitComment")
     
-    data = {
+    form = {
         "auth": self.auth_token,
         "content": content,
         "post_id": post_id,
@@ -12,14 +12,14 @@ def submitComment(self, post_id, content, parent_id=None, instance=None, title=N
     }
     
     if language_id:
-        data["language_id"] = language_id
+        form["language_id"] = language_id
     if parent_id:
-        data["parent_id"] = parent_id
+        form["parent_id"] = parent_id
     
     res = self._req.request(
         HttpType.POST,
         url,
-        data,
+        form,
     )
     
     return res
