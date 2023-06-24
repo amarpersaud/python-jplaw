103d57bfa2511a94b3c71e25e6362154e533bc90
Amar Persaud <tehspartaa@gmail.com>
2023-06-24 18:18:02 -0400

    Add language type

---
+++ jplaw/language.py
@@ -0,0 +1,8 @@
+class Language():
+    id: int
+    code: str
+    name: str
+    def __init__(language_id:int, code:str, name:str):
+        self.id = language_id
+        self.code = code
+        self.name = name
