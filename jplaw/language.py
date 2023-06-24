6aa172d41634a95ed324ffa3b36b3756c07f7611
Amar Persaud <tehspartaa@gmail.com>
2023-06-24 18:23:48 -0400

    Missing self in Language

--- jplaw/language.py
+++ jplaw/language.py
@@ -2,7 +2,7 @@ class Language():
     id: int
     code: str
     name: str
-    def __init__(language_id:int, code:str, name:str):
+    def __init__(self, language_id:int, code:str, name:str):
         self.id = language_id
         self.code = code
         self.name = name
