f6d6e93698e3ea3282e735ddd9b858d2ee06009c
Amar Persaud <tehspartaa@gmail.com>
2023-06-25 16:30:31 -0400

    Implement site parameters

--- jplaw/requestor.py
+++ jplaw/requestor.py
@@ -106,7 +106,7 @@ class Requestor:
         res_data = self.lemmyRequest("login", form=form, instance=instance)
         return res_data["jwt"]
     
-    def AddIfValue(self, name, value, form):
+    def AddIfValue(self, name:str, value, form: Dict[str, Any]):
         """
         Adds items to form if not None
         Args:
