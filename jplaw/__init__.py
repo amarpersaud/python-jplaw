e8e1010e23b8f5c640bea65ecd5cc25658f9d76d
Amar Persaud <tehspartaa@gmail.com>
2023-07-02 01:21:33 -0400

    Bump version

--- jplaw/__init__.py
+++ jplaw/__init__.py
@@ -1,7 +1,7 @@
 """
     The jplaw library is designed to allow interactions with the API of Lemmy instances. For function access, see the submodules. For More information on the API, see the Lemmy API documentation.
 """
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 """Current package version"""
 
 __package__ = "jplaw"
